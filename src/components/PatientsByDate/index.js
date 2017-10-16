42e021f39d25c7e3fc1841caf165c1c52457a047
Brad Conley <brad.conl3y@gmail.com>
2017-10-15 20:17:18 -0400

    debugging

--- src/components/PatientsByDate/index.js
+++ src/components/PatientsByDate/index.js
@@ -57,7 +57,7 @@ class PatientsByDate extends Component {
   }
 
   render() {
-    console.log(this.state.locations)
+    console.log(process.env.REACT_APP_GMAPS_API)
     return (
       <div>
       <h5>Appointments for:</h5>
