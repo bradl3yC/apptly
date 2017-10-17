da40fc25960487ef3bba1933ae78bd66174dcc88
Brad Conley <brad.conl3y@gmail.com>
2017-10-17 17:31:59 -0400

    Add spacing to logo silly materialize

--- src/components/LoggedIn/index.js
+++ src/components/LoggedIn/index.js
@@ -12,7 +12,7 @@ const logOut = () => {
 const LoggedIn = () => (
   <nav className="blue darken-4">
     <div className="nav-wrapper">
-      <Link to="/" className="brand-logo apptly">Apptly</Link>
+      <Link to="/" className="brand-logo apptly">&nbsp;&nbsp;Apptly</Link>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li>
           <Link to="/patients_by_date">Today's Patients</Link>
--- src/components/LoggedOut/index.js
+++ src/components/LoggedOut/index.js
@@ -7,7 +7,7 @@ import './index.css';
 const LoggedOut = (props) => (
   <nav className="blue darken-4">
     <div className="nav-wrapper">
-      <Link to="/" className="brand-logo apptly">Apptly</Link>
+      <Link to="/" className="brand-logo apptly">&nbsp;&nbsp;Apptly</Link>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><Link to="/login">Log In!</Link></li>
       </ul>
